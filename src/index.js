5e3b5326bfbe9764d77aa8d5193698d5b8d20c28
BandsWithLegends <adamosimpson21@gmail.com>
2018-08-14 00:56:09 -0700

    deploying to heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+//off for deployment
+// import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-registerServiceWorker();
+// registerServiceWorker();
