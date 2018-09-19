4b73f481330cef71f0b337dd868e00ad0142d260
BandsWithLegends <adamosimpson21@gmail.com>
2018-09-19 00:32:27 -0700

    # Please include a useful commit message!

--- src/leaderboard/RunDescription.js
+++ src/leaderboard/RunDescription.js
@@ -9,14 +9,18 @@ class RunDescription extends Component {
     return (
       <div className='runDescription'>
         <div className='innerSpan'>
-          <span className='runDescriptionPlace'>{place}. </span>
-          <div className='playerDisplay2'><Player id={runner[0].id} />
+          <div className='playerDisplayTopLine'>
+            <span className='runDescriptionPlace'>{place}. </span>
+            <div className='playerDisplay2'><Player id={runner[0].id} />
+          </div>
             {runner[1] && <span className='Player2Display'> and <Player id={runner[1].id} /></span>}
           </div>
-          <Link className="linkToSpeedrun" to={'/speedrun/' + id}>
-            <div className="timeDisplay">{time} </div>
-            <span>{date} </span>
-          </Link>
+          <div className='playerDisplayBottomLine'>
+            <Link className="linkToSpeedrun" to={'/speedrun/' + id}>
+              <div className="timeDisplay">{time} </div>
+              <span>{date} </span>
+            </Link>
+          </div>
         </div>
       </div>
     )
--- src/leaderboard/SideBar.js
+++ src/leaderboard/SideBar.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import './SideBar.css'
 import Button from '../innerComponents/Button'
 import BackFrame from '../innerComponents/BackFrame'
+import {Grid, Row, Col} from 'react-flexbox-grid';
 
 class SideBar extends Component {
   isPressed(boolean){
@@ -11,16 +12,20 @@ class SideBar extends Component {
     const {diff, seed, numPlayers} = this.props
     return (
       <div className='sideBar'>
-        <Button label='Normal' className={this.isPressed(diff==='Normal')} handleClick={(e) => this.props.handler(e, 'Normal')} />
-        <Button label='Expert' className={this.isPressed(diff==='Expert')} handleClick={(e) => this.props.handler(e, 'Expert')} />
-        <br />
-        <br />
-        <Button label='Seeded' className={this.isPressed(seed==='Seeded')} handleClick={(e) => this.props.handler(e, 'Seeded')} />
-        <Button label='Random' className={this.isPressed(seed==='Random')} handleClick={(e) => this.props.handler(e, 'Random')} />
-        <br />
-        <br />
-        <Button label='1 Player' className={this.isPressed(numPlayers==='1 Player')} handleClick={(e) => this.props.handler(e, '1 Player')} />
-        <Button label='2 Player' className={this.isPressed(numPlayers==='2 Players')} handleClick={(e) => this.props.handler(e, '2 Players')} />
+        <Grid>
+          <Row className='sideBarVariableRow'>
+            <Button label='Normal' className={this.isPressed(diff==='Normal')} handleClick={(e) => this.props.handler(e, 'Normal')} />
+            <Button label='Expert' className={this.isPressed(diff==='Expert')} handleClick={(e) => this.props.handler(e, 'Expert')} />
+          </Row>
+          <Row className='sideBarVariableRow'>
+            <Button label='Seeded' className={this.isPressed(seed==='Seeded')} handleClick={(e) => this.props.handler(e, 'Seeded')} />
+            <Button label='Random' className={this.isPressed(seed==='Random')} handleClick={(e) => this.props.handler(e, 'Random')} />
+          </Row>
+          <Row className='sideBarVariableRow'>
+            <Button label='1 Player' className={this.isPressed(numPlayers==='1 Player')} handleClick={(e) => this.props.handler(e, '1 Player')} />
+            <Button label='2 Player' className={this.isPressed(numPlayers==='2 Players')} handleClick={(e) => this.props.handler(e, '2 Players')} />
+          </Row>
+        </Grid>
       </div>
     )
   }
